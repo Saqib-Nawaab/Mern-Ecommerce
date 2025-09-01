6f758af24f5562a27de8d921d0038cd56a54f9f6
AQIB-NAWAB <aqibnawab1100@gmail.com>
2025-09-01 09:26:52 -0700

    da

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "http://localhost:8000/api/v2";
+export const server = "https://mern-ecommerce-backend-kappa.vercel.app/api/v2";
 
-export const backendUrl = "http://localhost:8000";
+export const backendUrl = "https://mern-ecommerce-backend-kappa.vercel.app";
