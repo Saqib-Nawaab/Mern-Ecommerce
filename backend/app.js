3e01a8b81cd5cdcef6b3f63fe2b7a2d2d9f6be8b
AQIB-NAWAB <aqibnawab1100@gmail.com>
2025-09-01 09:29:44 -0700

    fixed cors

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ const __dirname = dirname(__filename);
 
 const app = express();
 
-app.use(cors({origin: "http://localhost:5173",credentials: true,}));
+app.use(cors({origin: "https://mern-ecommerce-frontend-rust.vercel.app",credentials: true,}));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
