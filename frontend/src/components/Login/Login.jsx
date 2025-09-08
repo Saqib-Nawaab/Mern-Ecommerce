d52ecb7f70ba8d71cd74871b2f768e4592d01e7d
AQIB-NAWAB <aqibnawab1100@gmail.com>
2025-09-08 10:48:48 -0700

    correct the mistakes

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -121,20 +121,6 @@ const Login = () => {
                   id="remember-me"
                   className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
                 />
-                <label
-                  htmlFor="remember-me"
-                  className="ml-2 block text-sm text-gray-900"
-                >
-                  Remember me
-                </label>
-              </div>
-              <div className="text-sm">
-                <a
-                  href=".forgot-password"
-                  className="font-medium text-blue-600 hover:text-blue-500"
-                >
-                  Forgot password?
-                </a>
               </div>
             </div>
             <div>
